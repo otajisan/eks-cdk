72ad24c0dde09ced7926f9b08b8c5d9facaade63
otajisan <taji.taji.otajisan@gmail.com>
2022-01-04 16:52:00 +0900

    :+1: add EKS cluster creation

--- lib/eks-cdk-stack.ts
+++ lib/eks-cdk-stack.ts
@@ -1,9 +1,39 @@
 import * as cdk from '@aws-cdk/core';
+import {ManagedPolicy, Role, ServicePrincipal} from "@aws-cdk/aws-iam";
+import {Cluster, KubernetesVersion} from "@aws-cdk/aws-eks";
+import {Vpc} from "@aws-cdk/aws-ec2";
+import {Tags} from "@aws-cdk/core";
 
 export class EksCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // VPC
+    const vpc = Vpc.fromLookup(this, 'Vpc', {
+      vpcName: 'morningcode-main-vpc',
+    });
+
+    // IAM Role for EKS
+    const eksRole = new Role(this, 'EksRole', {
+      roleName: 'eks-role',
+      assumedBy: new ServicePrincipal('eks.amazonaws.com')
+    });
+    eksRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('AmazonEKSClusterPolicy'));
+    eksRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('AmazonEKSServicePolicy'));
+
+    // EKS Cluster
+    const cluster = new Cluster(this, 'cluster', {
+      vpc: vpc,
+      mastersRole: eksRole,
+      clusterName: 'morningcode',
+      version: KubernetesVersion.V1_21,
+    });
+
+    cluster.addFargateProfile('FargateProfile', {
+      fargateProfileName: 'morningcode-eks-fargate-profile',
+      selectors: [{namespace: 'default'}],
+    });
+
+    Tags.of(this).add('ServiceName', 'morningcode');
   }
 }
