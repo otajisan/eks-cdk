d1069504e1a07a52d2db4221a2be7093ce0e7fa3
otajisan <taji.taji.otajisan@gmail.com>
2022-01-06 22:56:24 +0900

    :+1: fix entrypoint

--- .github/actions/aws-cdk-update-checker/entrypoint.sh
+++ .github/actions/aws-cdk-update-checker/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-latest_version=$(aws_cdk_update_checker)
-echo "::set-output name=latest_version::$latest_version"
+echo "::set-output name=latest_version::$(aws_cdk_update_checker)"
 echo "::set-output name=foo::bar"
 exit 0
